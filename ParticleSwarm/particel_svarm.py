229c0a0f8ff23b51ce615cfbbc8097d59f8f3e64
Klas Holmgren <klas0holmgren@gmail.com>
2023-03-27 15:54:05 +0200

    work on particle swarm by adding constants to update vel and translating functions to particleSwarm class

--- ParticleSwarm/particel_svarm.py
+++ ParticleSwarm/particel_svarm.py
@@ -2,115 +2,11 @@ import numpy as np
 import pandas as pd
 from particle import initialize_swarm
 from target_function import get_funtion_val
-
-
-def PSO(swarm_size=50, itervall = [-5, 5]):
-  """
-  Particle Swarm Optimization
-
-  Args:
-    swarm_size -
-    itervall -
-
-  Returns:of function:
-    minima
-  """
-  
-  particle_swarm = initialize_swarm(swarm_size, itervall)
-
-  minima = run_swarm_optimization(particle_swarm)
-
-  return(minima)
-
-
-def run_swarm_optimization(particel_swarm, time=1, inertiaConst = 1.4):
-  """
-  Args:
-    particel_swarm
-    time=1
-    inertiaConst= 1.4
-
-  Returns:
-    theminimaOfthaMinimima
-  """
-    
-  swarm_min_pos = particel_swarm.loc[1]
-  swarm_min = float('inf')
-
-  iter = 0
-  while(swarm_min > 0.0001):
-    iter = iter + 1
-    inertiaConst = inertiaConst*0.99
-    if(inertiaConst < 0.35):
-      inertiaConst = 0.35
-
-    particle_swarm, swarm_move_min = move_swarm(particel_swarm)
-
-    if(swarm_min < swarm_move_min):
-      best_found_particel = swarm_min
-      print("Iteration {iter}")
-      print("New Best X,Y")
-      print([best_found_particel(5), best_found_particel(1), best_found_particel(2)])
-
-    particle_swarm = update_swarms_best(particle_swarm,best_found_particel,inertiaConst)
-    theminimaOfthaMinimima = best_found_particel
-
-  return(theminimaOfthaMinimima)
-
-
-
-def update_swarms_best(particle_swarm,swarmBest,inertiaConst, cognetive_const = 2, social_const = 2):
-  """
-  
-  """
-  cognetive_const = 2
-  social_const = 2
-  timeStep = 1
-  vMax = 3/5
-
-  for particel in particle_swarm:
-    particle_best_point = particel['BestPoint']
-    xSwarmBest = swarmBest(6)
-    ySwarmBest = swarmBest(7)
-
-    # Cognetive_const
-    accelerartion = np.random.uniform(0,1)*cognetive_const*(particle_best_point-particle_swarm['position'])
-
-    # social_const
-    accelerartion += np.random.uniform(0,1)*social_const*(xSwarmBest-particle_swarm['position'])
-
-    # InertiaConst
-    accelX = accelX + inertiaConst*particel['velocity']
-
-    particel['acceleration'] = accelerartion
-
-    # Restricting Velocities
-    if(abs(particel['velocity'][0]) > vMax):
-      if(particel['velocity'][0] < 0):
-        particel['velocity'][0] = -vMswarm_ax
-      else:
-        particel['velocity'][0] = vMax
-
-
-    if(abs(particel['velocity'][1]) > vMax):
-      if(abs(particel['velocity'][1]) < 0):
-        particel['velocity'][1] = -vMax 
-      else:
-        particel['velocity'][1] = vMax
-
-    func = get_funtion_val(particel['pos'])
-
-    if func <  particel['MinValue']:
-      particel['MinValue'] = func
-      particel['MinPosition'] = particel['pos']
-
-  return(particle_swarm)
-
-
 import particle
 
+
 class ParticleSwarm():
-  
+
   def __init__(self) -> None:
     NumberOfParticles = 50
     dimensions = 2
@@ -155,7 +51,7 @@ class ParticleSwarm():
       #Treating Optimization as minimazation problem
       if particel_value < self.swarm_optimal_value:
         self.swarm_optimal_value = particel_value
-    
+        
   def run_optimization(self, objective_func):
     """
     Optimize objective function
--- ParticleSwarm/particle.py
+++ ParticleSwarm/particle.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import math
 
 class particle():
     """
@@ -8,35 +9,62 @@ class particle():
     #TODO make N dimensional
     """
 
-    def __init__(self):
-        self.pos = (0, 0)
-        self.vel = (0, 0)
+    def __init__(self, lower_bound, upper_bound, n):
+        """
+        Initilizes Position and Velocity of the particle.
+        """
+        self.pos = np.random.uniform(lower_bound, upper_bound, size=(n,))
+        vel_bound = upper_bound - lower_bound
+        self.vel = np.random.uniform(-vel_bound, vel_bound, size=(n,))
         self.optima = None
+        self.varibale_optima = None
         
     def move(self):
         self.pos += self.vel
 
-    def upadte_velocity(self, optima, max_velocity):
+    def upadte_velocity(self, optima,
+                        max_velocity,
+                        inertia_constant = 0.7,
+                        cognetuive_constant = 2,
+                        social_constant = 2):
         """
-        aimes the particel towards optima
+        aimes the particel towards optima acording to
+        vi,d ← w vi,d + φp rp (pi,d-xi,d) + φg rg (gd-xi,d)
         """
 
-        # Restricting Velocities
-        # Cap the value from below
-        value = max(self, min_value)
+        #np.random.uniform(0,1)
+        self.vel = (inertia_constant*self.vel[0], inertia_constant*self.vel[1])
+
+        # Cognative constant
+        rand = np.random.uniform(0,1)
+        self.vel = (self.vel[0] + rand*cognetuive_constant*self.vel[0], self.vel[1] + rand*cognetuive_constant*self.vel[1])
+
+        # Social Constant
+        rand = np.random.uniform(0,1)
+        self.vel = (self.vel[0] + rand*social_constant*self.vel[0], self.vel[1] + rand*social_constant*self.vel[1])
 
         # Cap the value from above
-        value = min(value, max_value)
+        magnitude = math.sqrt(self.vel[0]**2 + self.vel[1]**2)
+     
+        if magnitude > max_velocity:
+            # Calculate the scaling factor to preserve the direction of the vector
+            scale_factor = max_velocity/magnitude
+
+            # Scale down the vector components
+            self.vel = (self.vel[0]*scale_factor, self.vel[1]*scale_factor)
 
     def upadte_value(self, objective_function):
+        """
+        Update the value of the particle
+        """
+
         self.value = objective_function(*self.pos)
+        
         if self.value < self.optima:
-            self.optima = self.value()
+            self.optima = self.value
+            self.varibale_optima = self.pos
         
         return self.optima
-
-
-
 
 
 def initialize_swarm(swarm_size, itervall):
